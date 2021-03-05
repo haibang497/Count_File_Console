4afde8d8d91f927ac643d4700e5c3f8787a38ff1
haibang497 <haibang497@gmail.com>
2021-03-05 13:58:37 +0700

    write result to file text.txt

--- CountFile/Program.cs
+++ CountFile/Program.cs
@@ -13,13 +13,13 @@ namespace CountFile
         {
             try
             {
-                String file1Path = @"D:\ExportExcelWebERP";
+                String file1Path = @"D:\WorkTGDD\A\C";
                 DirectoryInfo directory1 = new DirectoryInfo(file1Path);
                 FileInfo[] file1 = directory1.GetFiles("*", SearchOption.AllDirectories);
                 Console.Write("Total file in {0}: ", file1Path);
                 Console.WriteLine(file1.Length.ToString());
 
-                String file2Path = @"D:\ExportExcelWebERP - Copy";
+                String file2Path = @"D:\WorkTGDD\B\C";
                 DirectoryInfo directory2 = new DirectoryInfo(file2Path);
                 FileInfo[] file2 = directory2.GetFiles("*", SearchOption.AllDirectories);
                 Console.Write("Total file in {0}: ", file2Path);
@@ -37,25 +37,32 @@ namespace CountFile
                     Console.WriteLine("=> Two folders are not the same");
                 }
 
-                var queryCommonfiles = file1.Intersect(file2, fileCompare);
+                //var queryCommonfiles = file1.Intersect(file2, fileCompare);
 
-                if (queryCommonfiles.Any())
-                {
-                    Console.WriteLine("The following files are in both folders:");
-                    foreach (var v in queryCommonfiles)
-                    {
-                        Console.WriteLine(v.FullName);
-                    }
-                }
+                //if (queryCommonfiles.Any())
+                //{
+                //    Console.WriteLine("The following files are in both folders:");
+                //    foreach (var v in queryCommonfiles)
+                //    {
+                //        Console.WriteLine(v.FullName);
+                //    }
+                //}
+
+
+                string file_name = "D:\\WorkTGDD\\CountFile\\text.txt";
+                StreamWriter streamWriter = new StreamWriter(file_name);
+                streamWriter.WriteLine("Total file in {0}: {1}", file1Path, file1.Length);
+                streamWriter.WriteLine("Total file in {0}: {1}", file2Path, file2.Length);
+                streamWriter.WriteLine("The following files are in folder 1 but not in folder 2:");
 
                 var queryFile1Only = (from file in file1
                                       select file).Except(file2, fileCompare);
-
-                Console.WriteLine("The following files are in folder 1 but not in folder 2:");
                 foreach (var v in queryFile1Only)
                 {
-                    Console.WriteLine(v.FullName);
+                    streamWriter.WriteLine(v.FullName);
                 }
+                streamWriter.Close();
+                Console.WriteLine("Success");
             }
             catch
             {
